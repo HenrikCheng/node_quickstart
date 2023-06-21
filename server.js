9338f832d6d124b8e1dfac1f43b64947c4de04fc
HenrikCheng <henrikcheng@live.se>
2023-06-21 11:56:10 +0200

    add base mongodb config

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
-
-
-app.listen(3000, () => {
-  console.log("Node API app is running on port: 3000");
+//routes
+app.get("/", (req, res) => {
+  res.send("Hello NODE API");
 });
+
+app.get("/blog", (req, res) => {
+  res.send("Hello Blog");
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:Pretender00!N@devhenrikapi.w2kqe4x.mongodb.net/Node-API?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(3000, () => {
+      console.log("Node API app is running on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log("error: ", error);
+  });
