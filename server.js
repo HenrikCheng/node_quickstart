18c96046102d8c52742866e1874bca43c1456a52
HenrikCheng <henrikcheng@live.se>
2023-06-27 09:43:43 +0200

    test commit after moving folder locally

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
+// app.use(express.urlencoded({ extended: false })); // Different language to JSON
 
 //routes
 app.get("/", (req, res) => {
